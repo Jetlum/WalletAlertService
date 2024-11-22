0f85fd793129a46530cbff5a6e6ffdb32e05c2b1
Jetlum <jetlumajeti@gmail.com>
2024-11-22 19:35:04 +0100

    Create event model structure

---
+++ event.go
@@ -0,0 +1,13 @@
+package models
+
+import "gorm.io/gorm"
+
+type Event struct {
+	gorm.Model
+	TxHash      string `gorm:"uniqueIndex"`
+	FromAddress string `gorm:"index"`
+	ToAddress   string `gorm:"index"`
+	Value       string `gorm:"type:numeric"`
+	EventType   string `gorm:"index"`
+	Notified    bool
+}
