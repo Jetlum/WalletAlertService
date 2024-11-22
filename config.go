4246d33970fd62a3d463259c29bebc1cf5b83766
Jetlum <jetlumajeti@gmail.com>
2024-11-22 19:25:41 +0100

    Add configuration management

--- config.go
+++ config.go
@@ -1,15 +1,27 @@
 package config
 
+import (
+	"github.com/spf13/viper"
+)
+
 type Config struct {
 	InfuraProjectID string
 	DatabaseURL     string
 	SendGridAPIKey  string
 }
 
-func LoadConfig() Config {
-	return Config{
-		InfuraProjectID: "YOUR_INFURA_PROJECT_ID",
-		DatabaseURL:     "DatabaseURL",
-		SendGridAPIKey:  "SendGridAPIKey",
+func LoadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
 	}
+
+	return &Config{
+		InfuraProjectID: viper.GetString("infura.project_id"),
+		DatabaseURL:     viper.GetString("database.url"),
+		SendGridAPIKey:  viper.GetString("sendgrid.api_key"),
+	}, nil
 }
