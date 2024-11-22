a9a15d02c97a7d6ed662f023a9fb0cd4b5359d92
Jetlum <jetlumajeti@gmail.com>
2024-11-22 19:25:24 +0100

    Add user preference structure

---
+++ models.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type UserPreference struct {
+	gorm.Model
+	UserID            string `gorm:"uniqueIndex"`
+	WalletAddress     string `gorm:"index"`
+	MinEtherValue     string `gorm:"type:numeric"`
+	TrackNFTs         bool
+	EmailNotification bool
+	SMSNotification   bool
+}
