f9ca04a3c810ce5f819f78505e3f5952b3894faa
Jetlum <jetlumajeti@gmail.com>
2024-11-23 22:14:29 +0100

    Fix struct field name in EmailNotification initialization

--- services/notification.go
+++ services/notification.go
@@ -4,34 +4,16 @@ import (
 	"fmt"
 
 	"github.com/Jetlum/WalletAlertService/models"
-
-	"github.com/sendgrid/sendgrid-go"
-	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 type NotificationService interface {
 	Send(event *models.Event, userPreference *models.UserPreference) error
 }
 
-type EmailNotification struct {
-	apiKey string
-}
-
 func NewEmailNotification(apiKey string) *EmailNotification {
-	return &EmailNotification{apiKey: apiKey}
-}
-
-func (n *EmailNotification) Send(event *models.Event, userPref *models.UserPreference) error {
-	from := mail.NewEmail("Wallet Alert", "alerts@yourdomain.com")
-	subject := "New Wallet Activity Detected"
-	to := mail.NewEmail("User", userPref.UserID)
-	content := mail.NewContent("text/plain", formatEventMessage(event))
-
-	message := mail.NewV3MailInit(from, subject, to, content)
-	client := sendgrid.NewSendClient(n.apiKey)
-
-	_, err := client.Send(message)
-	return err
+	return &EmailNotification{
+		APIKey: apiKey,
+	}
 }
 
 func formatEventMessage(event *models.Event) string {
