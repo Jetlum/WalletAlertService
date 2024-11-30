3ddda1c1e2c6e9a7d563319e921556fdbc9382fd
Jetlum <jetlumajeti@gmail.com>
2024-11-30 21:08:37 +0100

    Implement EventRepository with Create, GetUnnotifiedEvents, and MarkAsNotified methods

--- repository/event_repository.go
+++ repository/event_repository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"fmt"
+
 	"github.com/Jetlum/WalletAlertService/models"
 	"gorm.io/gorm"
 )
@@ -13,6 +15,19 @@ func NewEventRepository(db *gorm.DB) *EventRepository {
 	return &EventRepository{db: db}
 }
 
-func (r *EventRepository) Create(event *models.Event) error {
-	return r.db.Create(event).Error
+func (er *EventRepository) Create(event *models.Event) error {
+	if event == nil {
+		return fmt.Errorf("event cannot be nil")
+	}
+	return er.db.Create(event).Error
+}
+
+func (er *EventRepository) GetUnnotifiedEvents() ([]models.Event, error) {
+	var events []models.Event
+	err := er.db.Where("notified = ?", false).Find(&events).Error
+	return events, err
+}
+
+func (er *EventRepository) MarkAsNotified(eventID uint) error {
+	return er.db.Model(&models.Event{}).Where("id = ?", eventID).Update("notified", true).Error
 }
