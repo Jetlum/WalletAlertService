e216e8aaa25c46894031e592b61b97ba1c0c9b29
Jetlum <jetlumajeti@gmail.com>
2024-12-07 20:52:12 +0100

    Update model to include price alert preferences

--- models/models.go
+++ models/models.go
@@ -4,6 +4,18 @@ import (
 	"gorm.io/gorm"
 )
 
+// Add new model for price alerts
+type PriceAlert struct {
+	gorm.Model
+	UserID            string `gorm:"index"`
+	CryptocurrencyID  string `gorm:"index"`        // e.g., "BTC", "ETH"
+	ThresholdPrice    string `gorm:"type:numeric"` // Store as string for precision
+	IsUpperBound      bool   // true for price above, false for below
+	IsActive          bool   `gorm:"default:true"`
+	EmailNotification bool   `gorm:"default:true"`
+}
+
+// Update UserPreference to include price alerts
 type UserPreference struct {
 	gorm.Model
 	UserID            string `gorm:"uniqueIndex"`
@@ -12,4 +24,5 @@ type UserPreference struct {
 	TrackNFTs         bool
 	EmailNotification bool
 	PushNotification  bool
+	PriceAlerts       []PriceAlert `gorm:"foreignKey:UserID"`
 }
